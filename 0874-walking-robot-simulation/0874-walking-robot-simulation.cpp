16ca133cd79f931bf739dbc126b21ab0891b3d4a
yash0260 <yash54y@gmail.com>
2026-04-06 17:10:27 +0530

    Time: 2618 ms (5.33%), Space: 33.2 MB (100%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -1,34 +1,53 @@
 class Solution {
 public:
-    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        set <pair<int,int>>obs;
-        for(auto &o:obstacles){
-            obs.insert({o[0],o[1]});
+    
+    // function to check obstacle using vector
+    bool isObstacle(int x, int y, vector<vector<int>>& obstacles){
+        for(auto &o : obstacles){
+            if(o[0] == x && o[1] == y){
+                return true;
+            }
         }
-        vector<pair<int,int>>dir={
-{0,1},{1,0},{0,-1},{-1,0}
+        return false;
+    }
+
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        
+        vector<pair<int,int>> dir = {
+            {0,1}, {1,0}, {0,-1}, {-1,0}
         };
-        int x=0,y=0;
-        int d=0;
-        int maxdist=0;
-        for(int cmd:commands){
-            if(cmd==-1){
-                d=(d+1)%4;
-            }else if (cmd==-2){
-d=(d+3)%4;
-            }else{
-                for(int i=0;i<cmd;i++){
-                    int nx=x+dir[d].first;
-                    int ny=y+dir[d].second;
-                    if(obs.count({nx,ny})){
+
+        int x = 0, y = 0;
+        int d = 0;
+        int maxdist = 0;
+
+        for(int cmd : commands){
+            
+            if(cmd == -1){
+                d = (d + 1) % 4;   // turn right
+            }
+            else if(cmd == -2){
+                d = (d + 3) % 4;   // turn left
+            }
+            else{
+                for(int i = 0; i < cmd; i++){
+                    
+                    int nx = x + dir[d].first;
+                    int ny = y + dir[d].second;
+
+                    // check using vector
+                    if(isObstacle(nx, ny, obstacles)){
                         break;
                     }
-                    x=nx;
-                    y=ny;
-                    maxdist=max(maxdist,x*x+y*y);
+
+                    x = nx;
+                    y = ny;
+
+                    maxdist = max(maxdist, x*x + y*y);
                 }
             }
         }
+
         return maxdist;
     }
 };
