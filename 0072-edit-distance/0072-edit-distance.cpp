6c57c46a5c03d94e9b4383a47b873fea7db984a2
yash0260 <yash54y@gmail.com>
2026-06-19 14:24:16 +0530

    Time: 3 ms (91.57%), Space: 13.2 MB (58.77%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+            for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (word1[i - 1] == word2[j - 1]) {
+                // If the characters match, no additional cost
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                // Minimum of three choices:
+                // 1. Replace the character at S1[i-1] with S2[j-1]
+                // 2. Delete the character at S1[i-1]
+                // 3. Insert the character at S2[j-1] into S1
+                dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
+            }
+        }
+    }
+
+    // The value at dp[n][m] contains the edit distance
+    return dp[n][m];
+}
+};
