2a2a8601211788a1e647ecfdb4f3cec23aea1e77
yash0260 <yash54y@gmail.com>
2026-07-21 23:23:02 +0530

    Time: 5 ms (10.97%), Space: 16.3 MB (44.47%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,33 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>>nodes;
-        queue<pair<TreeNode*,pair<int,int>>>todo;
-        todo.push({root,{0,0}});
-        while(!todo.empty()){
-            auto p=todo.front();
-            todo.pop();
-            TreeNode* node=p.first;
-            int x=p.second.first,y=p.second.second;
-             nodes[x][y].insert(node->val);
-            if(node->left){
-                todo.push({node->left,{x-1,y+1}});
-            }
-            if(node->right){
-                todo.push({node->right,{x+1,y+1}});
-            }
+
+        map<int, map<int, multiset<int>>> nodes;
+
+        queue<pair<TreeNode*, pair<int,int>>> q;
+
+        q.push({root,{0,0}});
+
+        while(!q.empty()){
+
+            auto p = q.front();
+            q.pop();
+
+            TreeNode* node = p.first;
+
+            int row = p.second.first;
+            int col = p.second.second;
+
+            nodes[col][row].insert(node->val);
+
+            if(node->left)
+                q.push({node->left,{row+1,col-1}});
+
+            if(node->right)
+                q.push({node->right,{row+1,col+1}});
         }
+
         vector<vector<int>>ans;
         for(auto p:nodes){
             vector<int>col;
