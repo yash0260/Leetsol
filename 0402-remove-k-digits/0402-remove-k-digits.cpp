4b548bfd5ea9956bbdeb723d9f0436f37fd66d28
yash0260 <yash54y@gmail.com>
2026-08-07 17:39:03 +0530

    Time: 7 ms (24.82%), Space: 11.8 MB (21.97%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
-    string removeKdigits(string num, int k) {
-        stack<char> st;  // Stack to store digits
+    string removeKdigits(string s, int k) {
+     stack<char> st;
+        int n = s.size();
 
-        // Iterate through each digit in num
-        for (int i = 0; i < num.size(); i++) {
-            char c = num[i];
-
-            // Remove digits from stack if they are greater than current digit
-            while (!st.empty() && st.top() > c && k > 0) {
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && st.top() > s[i] && k > 0) {
                 st.pop();
                 k--;
             }
-
-            // Push current digit onto stack (avoid leading zeros)
-            if (!st.empty() || c != '0') {
-                st.push(c);
-            }
+            st.push(s[i]);
         }
 
-        // Remove extra digits from the top of the stack if k > 0
-        while (!st.empty() && k > 0) {
+        // If k is still left, remove from the end
+        while (k > 0 && !st.empty()) {
             st.pop();
             k--;
         }
 
-        // Construct the final number from stack
-        string ans = "";
+        string res = "";
         while (!st.empty()) {
-            ans += st.top();
+            res += st.top();
             st.pop();
         }
 
-        // Reverse the result since stack stores in reverse order
-        reverse(ans.begin(), ans.end());
+        reverse(res.begin(), res.end());
 
-        // If the result is empty, return "0"
-        if (ans.empty()) {
-    return "0";  // If ans is empty, return "0"
-} 
-return ans; 
+        int i = 0;
+        while (i < res.size() && res[i] == '0') {
+            i++;
+        }
+
+        res = res.substr(i);
+
+        if (res.empty())
+            return "0";
+
+        return res;
     }
 };
