e677753321dc38be490251d4c18174fcd8517f58
yash0260 <yash54y@gmail.com>
2026-08-09 00:54:35 +0530

    Time: 0 ms (100%), Space: 9.5 MB (31.23%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
+int recursion(int i,int j, int m,int n,vector<vector<int>>&dp){
+if(i==m && j==n)return 1;
+if(i>m ||j>n)return 0;
+if(dp[i][j]!=-1)return dp[i][j];
+int right=recursion(i,j+1,m,n,dp);
+int bottom=recursion(i+1,j,m,n,dp);
+return dp[i][j]=right +bottom;
+}
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0){
-                    dp[i][j]=1;
-                }
-                else{
-                    int up = 0;
-                    int left = 0;
-
-                    if(i>0)up=dp[i-1][j];
-                    if(j>0)left=dp[i][j-1];
-
-                    dp[i][j]=up+left;
-
-                }
-            
-            }
-        }
-       return dp[m-1][n-1];
+       return recursion(0,0,m-1,n-1,dp);
     }
 };
