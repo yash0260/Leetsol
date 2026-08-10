1bd959c4b1fe48c904a48c78598fba4e52e7c54a
yash0260 <yash54y@gmail.com>
2026-08-10 20:14:04 +0530

    Time: 460 ms (12.23%), Space: 0B (100%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,8 @@
+SELECT employee_id
+FROM Employees e
+WHERE salary < 30000
+  AND manager_id IS NOT NULL
+  AND manager_id NOT IN (
+      SELECT employee_id
+      FROM Employees
+  ) Order by e.employee_id;
