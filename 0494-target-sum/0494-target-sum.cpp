25dc5acf413bd2750b542a9ffc6e0b8e72cd40d6
yash0260 <yash54y@gmail.com>
2026-08-12 10:25:58 +0530

    Time: 0 ms (100%), Space: 15.5 MB (49.89%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,35 +1,59 @@
 class Solution {
-    int f(int ind, int target, vector<int>& arr, vector<vector<int>>& dp){
-        if(ind==0){
-              if (target == 0 && arr[0] == 0)
-            return 2;
-              if (target == 0 || target == arr[0])
-            return 1;
-            return 0;
-        }
-         if (dp[ind][target] != -1)
-        return dp[ind][target];
-        
-    // Calculate the number of ways without taking the current element
-    int notTaken = f(ind - 1, target, arr, dp);
-    
-    // Calculate the number of ways by taking the current element
-    int taken = 0;
-    if (arr[ind] <= target)
-        taken = f(ind - 1, target - arr[ind], arr, dp);
-        
-    // Store the sum of ways in the DP array and return it
-    return dp[ind][target] = (notTaken + taken);
-
-    }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-       int total=accumulate(nums.begin(),nums.end(),0);
-       if(total-target<0) return 0;
-       if((total-target)%2==1) return 0;
-       int s2=(total-target)/2;
-       vector<vector<int>>dp(n,vector<int>(s2+1,-1));
-       return f(n-1,s2,nums,dp); 
+
+        int n = nums.size();
+
+        int total = accumulate(nums.begin(), nums.end(), 0);
+
+        if (total - target < 0)
+            return 0;
+
+        if ((total - target) % 2 != 0)
+            return 0;
+
+        int s2 = (total - target) / 2;
+
+        // dp[ind][sum] = number of ways to make 'sum'
+        // using nums[0...ind]
+        vector<vector<int>> dp(n, vector<int>(s2 + 1, 0));
+
+        // Base case: only nums[0] is available
+
+        if (nums[0] == 0) {
+            // {} and {0}
+            dp[0][0] = 2;
+        }
+        else {
+            // Empty subset makes sum 0
+            dp[0][0] = 1;
+
+            // Taking nums[0] makes sum nums[0]
+            if (nums[0] <= s2) {
+                dp[0][nums[0]] = 1;
+            }
+        }
+
+        // Fill the DP table
+        for (int ind = 1; ind < n; ind++) {
+
+            for (int sum = 0; sum <= s2; sum++) {
+
+                // Don't take nums[ind]
+                int notTaken = dp[ind - 1][sum];
+
+                // Take nums[ind]
+                int taken = 0;
+
+                if (nums[ind] <= sum) {
+                    taken = dp[ind - 1][sum - nums[ind]];
+                }
+
+                // Total number of ways
+                dp[ind][sum] = notTaken + taken;
+            }
+        }
+
+        return dp[n - 1][s2];
     }
 };
