441608ea4b66ce83029b5282e171ae0fdbeb2d0f
yash0260 <yash54y@gmail.com>
2026-08-22 02:03:14 +0530

    Time: 27 ms (55.89%), Space: 22.9 MB (32.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,32 +1,54 @@
 class Solution {
 public:
- void dfs(int row, int col, vector<vector<char>>& grid,int n,int m) {
-grid[row][col]='2';
-    int dr[]={-1,1,0,0};
-    int dc[]={0,0,-1,1};
-    for(int i=0;i<4;i++){
-int newr=row+dr[i];
-int newc=col+dc[i];
- if (newr >= 0 && newr < n && newc >= 0 && newc < m &&
-            grid[newr][newc] == '1')
-dfs(newr,newc,grid,n,m);
-    }
+
+    void bfs(int row, int col, vector<vector<char>>& grid, int n, int m) {
+
+        queue<pair<int, int>> q;
+
+        q.push({row, col});
+        grid[row][col] = '2';
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        while(!q.empty()) {
+
+            auto [r, c] = q.front();
+            q.pop();
+
+            for(int i = 0; i < 4; i++) {
+
+                int newr = r + dr[i];
+                int newc = c + dc[i];
+
+                if(newr >= 0 && newr < n &&
+                   newc >= 0 && newc < m &&
+                   grid[newr][newc] == '1') {
+
+                    grid[newr][newc] = '2';
+
+                    q.push({newr, newc});
+                }
+            }
+        }
     }
 
     int numIslands(vector<vector<char>>& grid) {
-         int n = grid.size();
+
+        int n = grid.size();
         int m = grid[0].size();
 
         int islands = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
 
-                if (grid[i][j] == '1') {
+                if(grid[i][j] == '1') {
+
                     islands++;
-                    dfs(i, j, grid,n,m);
-                }
 
+                    bfs(i, j, grid, n, m);
+                }
             }
         }
 
