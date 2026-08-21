7a39430edcdeec6dd988c5870b9078f793ab64dd
yash0260 <yash54y@gmail.com>
2026-08-21 23:23:25 +0530

    Time: 0 ms (100%), Space: 16.3 MB (22.9%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,28 +1,51 @@
 class Solution {
 public:
-int getans(vector<int>& prices,int ind,int buy,int n,vector<vector<int>>&dp){
-    if(ind>=n ){
-        return 0;
+    int solve(int ind, int buy, vector<int>& prices,
+              vector<vector<int>>& dp) {
+
+        // No days left
+        if(ind >= prices.size())
+            return 0;
+
+        // Already calculated
+        if(dp[ind][buy] != -1)
+            return dp[ind][buy];
+
+        int profit = 0;
+
+        if(buy) {
+
+            // Buy today
+            int take = -prices[ind] +
+                       solve(ind + 1, 0, prices, dp);
+
+            // Don't buy today
+            int nottake = solve(ind + 1, 1, prices, dp);
+
+            profit = max(take, nottake);
+
+        }
+        else {
+
+            // Sell today
+            int sell = prices[ind] +
+                       solve(ind + 2, 1, prices, dp);
+
+            // Don't sell today
+            int notsell = solve(ind + 1, 0, prices, dp);
+
+            profit = max(sell, notsell);
+        }
+
+        return dp[ind][buy] = profit;
     }
-    if(dp[ind][buy]!=-1){
-        return dp[ind][buy];
-    }
-    int profit=0;
-    if(buy==0){
-        profit=max(0+getans(prices,ind+1,0,n,dp),-prices[ind]+getans(prices,ind+1,1,n,dp));
-    }
-    if(buy==1){
-        profit=max(0+getans(prices,ind+1,1,n,dp),prices[ind]+getans(prices,ind+2,0,n,dp));
-    }
-    return dp[ind][buy]=profit;
-}
+
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        if(n==0){
-return 0;
-        }
-int ans=getans(prices,0,0,n,dp);
-return ans;
-        }
+
+        int n = prices.size();
+
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+
+        return solve(0, 1, prices, dp);
+    }
 };
