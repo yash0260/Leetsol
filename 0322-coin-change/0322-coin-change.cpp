100efaaa4503d814fb82694cf99642beac3958eb
yash0260 <yash54y@gmail.com>
2026-08-21 10:59:42 +0530

    Time: 45 ms (38.94%), Space: 49.1 MB (34.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
+int solve(int ind,vector<int>& coins, int amount,vector<vector<int>>&dp){
+    if(ind==0 && amount%coins[0]==0){
+        return amount/coins[0];
+    }else if(ind==0){
+        return 1e9;
+    }
+    if(dp[ind][amount]!=-1) return dp[ind][amount];
+    long take=INT_MAX;
+    long nottake=INT_MAX;
+    if(amount>=coins[ind]){
+        take=1+solve(ind,coins,amount-coins[ind],dp);
+    }
+    nottake=solve(ind-1,coins,amount,dp);
+    return dp[ind][amount]=min(take,nottake);
+}
     int coinChange(vector<int>& coins, int amount) {
-
-        int n = coins.size();
-
-        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
-
-        // Base case: using only coins[0]
-        for(int t = 0; t <= amount; t++) {
-
-            if(t % coins[0] == 0)
-                dp[0][t] = t / coins[0];
-            else
-                dp[0][t] = 1e9;
-        }
-
-        // Fill remaining rows
-        for(int ind = 1; ind < n; ind++) {
-
-            for(int t = 0; t <= amount; t++) {
-
-                // Don't take current coin
-                int nottake = dp[ind - 1][t];
-
-                // Take current coin
-                int take = INT_MAX;
-
-                if(coins[ind] <= t) {
-                    take = 1 + dp[ind][t - coins[ind]];
-                }
-
-                dp[ind][t] = min(take, nottake);
-            }
-        }
-
-        int ans = dp[n - 1][amount];
-
-        if(ans >= 1e9)
-            return -1;
-
-        return ans;
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans=solve(n-1,coins,amount,dp);
+        if(ans>=1e9)return -1;
+        else return ans;
     }
 };
