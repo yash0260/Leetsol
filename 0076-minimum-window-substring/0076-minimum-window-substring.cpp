f9a6907e5a1239feac23fa2d9d8eeee27201edbc
yash0260 <yash54y@gmail.com>
2026-08-23 16:18:39 +0530

    Time: 4 ms (70.42%), Space: 11.1 MB (98.89%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(t.size()>s.size()) return "";
-        unordered_map<char,int>tMap,windowMap;
-        for(char c:t)
-        tMap[c]++;
-        int required=tMap.size();
-        int formed=0;
-        int l=0,r=0;
-        int minLen=INT_MAX;
+        if(s.size()<t.size())return "";
+        vector<int>freq(128,0);
+        for(char c:t){
+            freq[c]++;
+        }
+        int left=0;
+        int right=0;
+        int required=t.size();
+        int minlen=INT_MAX;
         int start=0;
-        while(r<s.size()){
-            char c=s[r];
-            windowMap[c]++;
-            if(tMap.count(c) && windowMap[c]==tMap[c])
-            formed++;
-            while(l<=r && formed==required){
-                if(r-l+1<minLen){
-                    minLen=r-l+1;
-                    start=l;
-                }
-                char left=s[l];
-                windowMap[left]--;
-                 if (tMap.count(left) && windowMap[left] < tMap[left])
-                    formed--;
-
-                l++;
-
+        while(right<s.size()){
+            if(freq[s[right]]>0){
+                required--;
             }
-            r++;
-
+            freq[s[right]]--;
+            right++;
+            while(required==0){
+                if(right-left<minlen){
+                    minlen=right-left;
+                    start=left;
+                }
+                freq[s[left]]++;
+                if(freq[s[left]]>0){
+                    required++;
+                }
+                left++;
+            }
         }
-        if(minLen==INT_MAX){
-            return "";
-        }
-        return s.substr(start,minLen);
+        if(minlen==INT_MAX)return "";
+        return s.substr(start,minlen);
     }
 };
