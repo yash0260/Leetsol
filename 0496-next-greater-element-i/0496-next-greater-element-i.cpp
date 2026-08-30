7f416e16900eb062c9a774ff81f8b79d6b853456
yash0260 <yash54y@gmail.com>
2026-08-31 00:20:03 +0530

    Time: 3 ms (41.33%), Space: 12.4 MB (85.69%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,23 +1,50 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>nge;
-        stack<int>st;
-        for(int i=nums2.size()-1;i>=0;i--){
-            while(!st.empty()&& st.top()<=nums2[i]){
+
+        int n = nums2.size();
+        int m = nums1.size();
+
+        stack<int> st;
+
+        // next[i] = next greater element of nums2[i]
+        vector<int> next(n);
+
+        // Traverse from right to left
+        for(int i = n - 1; i >= 0; i--) {
+
+            // Remove elements which cannot be
+            // the next greater element
+            while(!st.empty() && st.top() <= nums2[i]) {
                 st.pop();
             }
-            if(!st.empty()){
-                nge[nums2[i]]=st.top();
-            }else{
-                nge[nums2[i]]=-1;
+
+            // Find answer
+            if(st.empty()) {
+                next[i] = -1;
             }
+            else {
+                next[i] = st.top();
+            }
+
+            // Push current element
             st.push(nums2[i]);
         }
-        vector<int>result;
-        for(int num:nums1){
-            result.push_back(nge[num]);
+
+        // Find nums1 elements inside nums2
+        for(int i = 0; i < m; i++) {
+
+            int k = nums1[i];
+
+            for(int j = 0; j < n; j++) {
+
+                if(nums2[j] == k) {
+                    nums1[i] = next[j];
+                    break;
+                }
+            }
         }
-        return result;
+
+        return nums1;
     }
 };
