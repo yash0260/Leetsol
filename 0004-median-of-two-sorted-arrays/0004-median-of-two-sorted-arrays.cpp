f30ffe312665e61197e383577103a3dffdb415cd
yash0260 <yash54y@gmail.com>
2026-09-01 10:27:26 +0530

    Time: 4 ms (18.91%), Space: 96.2 MB (9.54%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
-        int n = nums1.size();
-        int m = nums2.size();
 
-        // Step 1: Merge arrays
-        vector<int> merged;
-        for (int i = 0; i < n; i++) {
-            merged.push_back(nums1[i]);
+        vector<int> arr;
+
+        int i = 0;
+        int j = 0;
+        while(i < nums1.size() && j < nums2.size()) {
+
+            if(nums1[i] < nums2[j]) {
+                arr.push_back(nums1[i]);
+                i++;
+            }
+            else {
+                arr.push_back(nums2[j]);
+                j++;
+            }
         }
-        for (int i = 0; i < m; i++) {
-            merged.push_back(nums2[i]);
+        while(i < nums1.size()) {
+            arr.push_back(nums1[i]);
+            i++;
         }
+        while(j < nums2.size()) {
+            arr.push_back(nums2[j]);
+            j++;
+        }
+        int n = arr.size();
 
-        // Step 2: Sort merged array
-        sort(merged.begin(), merged.end());
-
-        // Step 3: Find median
-        int total = merged.size();
-        int mid = total / 2;
-
-        if (total % 2 == 1) {
-            return merged[mid];  // odd case
-        } else {
-            return (merged[mid - 1] + merged[mid]) / 2.0;  // even case
+        if(n % 2 == 1) {
+            return arr[n / 2];
+        }
+        else {
+            return (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
         }
     }
 };
