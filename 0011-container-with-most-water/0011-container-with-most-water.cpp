9d9266d064e856e98a887407059cfbf8ca59f43b
yash0260 <yash54y@gmail.com>
2026-09-02 23:28:12 +0530

    Time: 7 ms (6.52%), Space: 68 MB (11.2%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-    int left = 0;
-    int right = height.size() - 1;
-    int max_water = 0;
-
-    while (left < right) {
-        int width = right - left;
-        int h = min(height[left], height[right]);
-        max_water = max(max_water, width * h);
-
-        if (height[left] < height[right])
-            left++;
-        else
-            right--;
+        int n = height.size();
+        vector<int> pre(n, 0), post(n, 0); // prefix max , postfix max
+        pre[0] = height[0];
+        post[n - 1] = height[n - 1];
+        for (int i = 1; i < n; ++i) {
+            pre[i] = max(pre[i - 1], height[i]);
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            post[i] = max(post[i + 1], height[i]);
+        }
+        int i = 0, j = n - 1, mx = INT_MIN;
+        while (i < j) // two pointers concept
+        {
+            int area = min(pre[i], post[j]) * (j - i);
+            mx = max(mx, area);
+            if (pre[i] > post[j])
+                j--;
+            else
+                i++;
+        }
+        return mx;
     }
-
-    return max_water;
-}
 };
