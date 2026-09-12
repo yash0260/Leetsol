685afd9b0c7577de51c109ef00603e4617d4abbd
yash0260 <yash54y@gmail.com>
2026-09-12 10:06:23 +0530

    Time: 207 ms (18.76%), Space: 25.9 MB (27.94%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,29 +1,48 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n=s.size();
-        if(n==0) return "";
-        int start=0,maxlength=0;
-        for(int i=0;i<n;++i){
-            int left=i,right=i;
-            while(left>=0&& right<n && s[left] == s[right]){
-                if(right-left+1>maxlength){
-                    start=left;
-                    maxlength =right-left+1;
+        int n = s.size();
+
+        if (n == 0)
+            return "";
+
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        int start = 0;
+        int maxLen = 1;
+
+        // Length 1 substrings
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        // Check substrings of length 2 to n
+        for (int len = 2; len <= n; len++) {
+
+            for (int i = 0; i + len <= n; i++) {
+
+                int j = i + len - 1;
+
+                if (s[i] == s[j]) {
+
+                    // Length 2
+                    if (len == 2) {
+                        dp[i][j] = true;
+                    }
+                    // Length > 2
+                    else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
                 }
-                --left;
-                ++right;
-            }
-            left=i,right=i+1;
-            while(left>=0 && right <n && s[left] == s[right] ){
-                if (right - left + 1 > maxlength) {
-                    start = left;
-                    maxlength=right-left+1;
+
+                // Update longest palindrome
+                if (dp[i][j] && len > maxLen) {
+                    maxLen = len;
+                    start = i;
                 }
-                --left;
-                ++right;
             }
         }
-         return s.substr(start, maxlength);
+
+        return s.substr(start, maxLen);
     }
 };
