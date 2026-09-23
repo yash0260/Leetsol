22897c4da560bd1ea34434482a1578b6d76d896a
yash0260 <yash54y@gmail.com>
2026-09-23 22:45:27 +0530

    Time: 3 ms (37.2%), Space: 40.2 MB (80.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-
-        int n = nums.size();
-
-        vector<int> answer(n);
-
-        // Store left products
-        int left = 1;
-
-        for(int i = 0; i < n; i++) {
-            answer[i] = left;
-            left = left * nums[i];
+        int n=nums.size();
+        int ans=1;
+int cnt=0;
+int k=1;
+        for(int i=0;i<n;i++){
+            ans=nums[i]*ans;
+            if(nums[i]==0)cnt++;
+            if(nums[i]!=0){
+                k=k*nums[i];
+            }
         }
 
-        // Multiply by right products
-        int right = 1;
+        if(ans==0 && cnt>1){
+            for(int i=0;i<n;i++){
 
-        for(int i = n - 1; i >= 0; i--) {
-            answer[i] = answer[i] * right;
-            right = right * nums[i];
+                nums[i]=0;
+            }
+        }else if(cnt==1){
+for(int i=0;i<n;i++){
+    if(nums[i]==0)nums[i]=k;
+    else nums[i]=0;
+}
         }
 
-        return answer;
+        if(cnt==0){
+            for(int i=0;i<n;i++){
+                if(nums[i]!=0)nums[i]=ans/nums[i];
+            }
+        }
+        return nums;
     }
 };
