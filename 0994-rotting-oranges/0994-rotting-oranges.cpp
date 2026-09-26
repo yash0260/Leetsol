1eadfa912666c5280b1577b196da44ef80cd1a4b
yash0260 <yash54y@gmail.com>
2026-09-26 12:00:30 +0530

    Time: 0 ms (100%), Space: 16.8 MB (68.48%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,51 +1,54 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int ans=0;
-        int n=grid.size();
-        int m=grid[0].size();
-        int z=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if (grid[i][j]==1)z++;
-            }
-        }
-        if(z==0)return 0;
-        queue<pair<int,int>>q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-if(grid[i][j]==2){
-    q.push({i,j});
-}
-            }
-        }
-            int dr[]={-1,1,0,0};
-            int dc[]={0,0,-1,1};
-            while(!q.empty()){
-                int sz=q.size();
+       int n=grid.size();
+       int m=grid[0].size();
+       queue<pair<pair<int,int>,int>> q;
 
-    while(sz--){
-auto a=q.front();
-q.pop();
-int x=a.first;
-int y=a.second;
-for(int i=0;i<4;i++){
-   int  newr=x+dr[i];
-    int newc=y+dc[i];
-    if(newr>=0 && newr<n && newc>=0 && newc<m && grid[newr][newc]==1){
-        grid[newr][newc]=2;
-        q.push({newr,newc});
-    }
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(grid[i][j]==2){
+            q.push({{i,j},0});
             }
-            
         }
-        ans++;
-    }
+       }
+        int count = 0;
+
+        int dc[] = {-1,1,0,0};
+        int dr[] = {0,0,1,-1};
+
+        while(!q.empty()){
+
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            count = q.front().second;
+
+            q.pop();
+            for(int i=0;i<4;i++){
+
+                int newr = dr[i] + r;
+                int newc = dc[i] + c;
+
+                if(newr >= 0 && newr < n &&
+                   newc >= 0 && newc < m &&
+                   grid[newr][newc] == 1){
+
+                    grid[newr][newc] = 2;
+
+                    q.push({{newr,newc},count+1});
+                }
+            }
+        }
+
+        // Check if fresh oranges remain
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if (grid[i][j]==1)return -1;
+                if(grid[i][j] == 1){
+                    return -1;
+                }
             }
         }
-        return ans-1;
+
+        return count;
     }
 };
